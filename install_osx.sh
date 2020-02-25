821c821bb4b2f08442a4743e17fe6527fdd16ee0
mwicat <mwicat@gmail.com>
2020-02-25 21:27:11 +0100

    Add install script

---
+++ install_osx.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+LIVE_SCRIPTS_DIR="/Applications/Ableton Live 10 Suite.app/Contents/App-Resources/MIDI Remote Scripts"
+
+function install_script {
+    script_name="$1"
+    source_path="${SCRIPT_DIR}/${script_name}"
+    target_path="${LIVE_SCRIPTS_DIR}/${script_name}"
+    if [ -e "$target_path" ]; then
+        echo "Target path '$target_path' already exists"
+        return
+    fi
+    sudo ln -sfn "$source_path" "$target_path"
+    echo "Installed into '$target_path'"
+}
+
+install_script LiveOSC2
